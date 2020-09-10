973295e9e8e37bf4a3cc0fa08c8faf96d3081593
yangk <yangk@telsafe.com.cn>
2020-09-10 09:57:49 +0800

    update

---
+++ js/function.js
@@ -0,0 +1,19 @@
+function myAlert() {
+	window.alert("这是window.alert()弹框");
+}
+
+function myWrite() {
+	document.write(Date())
+}
+
+function myInner() {
+	document.getElementById("demo").innerHTML = "这是document.getElementById(#id).innerHTML";
+}
+
+function myLog() {
+	console.log("这是console.log()");
+}
+
+function myFunction() {
+	document.getElementById("demo").innerHTML = "我的第二个 JavaScript 函数";
+}
